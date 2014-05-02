f494ba9175b416ead59c5fb2a2e2b03678440ec2
karthikbadam <sriram.karthik6@gmail.com>
2014-05-02 01:46:21 -0400

    random adopted from d3

--- public/api/polychrome-panel.js
+++ public/api/polychrome-panel.js
@@ -40,6 +40,41 @@ eventCapture.mousedown = true;
 eventCapture.mousemove = true;
 eventCapture.mouseup = true;
 
+var pcrandom = {
+  normal: function(mu, sigma) {
+    var n = arguments.length;
+    if (n < 2) sigma = 1;
+    if (n < 1) mu = 0;
+    return function() {
+      var x, y, r;
+      do {
+        /* should synchronize this value!! for pseudo random */
+        x = Math.random() * 2 - 1;
+        y = Math.random() * 2 - 1;
+        r = x * x + y * y;
+      } while (!r || r > 1);
+      return mu + sigma * x * Math.sqrt(-2 * Math.log(r) / r);
+    };
+  },
+  logNormal: function() {
+    var random = d3.random.normal.apply(d3, arguments);
+    return function() {
+      return Math.exp(random());
+    };
+  },
+  bates: function(m) {
+    var random = d3.random.irwinHall(m);
+    return function() {
+      return random() / m;
+    };
+  },
+  irwinHall: function(m) {
+    return function() {
+      for (var s = 0, j = 0; j < m; j++) s += Math.random();
+      return s;
+    };
+  }
+};
 
 var FeedbackPanel = {
 
